eb52a7cc7848afc533639acedd4ac8c7371562ca
soldatkin <sanphir@yandex.ru>
2022-12-11 23:56:34 +0300

    Added simple validation to page size settings

--- BeeJee.Xamarin.App/BeeJee.Xamarin.App/ViewModels/SettingsViewModel.cs
+++ BeeJee.Xamarin.App/BeeJee.Xamarin.App/ViewModels/SettingsViewModel.cs
@@ -1,15 +1,32 @@
-﻿using Xamarin.Forms;
+﻿using BeeJee.Xamarin.App.Models;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace BeeJee.Xamarin.App.ViewModels
 {
     public class SettingsViewModel : BaseViewModel
     {
         private int _pagerPageSize = App.SettingsStore.PageSize.GetValue();
+        public ObservableCollection<ValidationError> ValidationErrors { get; } = new ObservableCollection<ValidationError>();
 
         public int PagerPageSize
         {
             get => _pagerPageSize;
-            set => SetProperty(ref _pagerPageSize, value);
+            set
+            {
+                SetProperty(ref _pagerPageSize, value);
+                if (ValidationErrors.Count > 0)
+                {
+                    ValidationErrors.Clear();
+                }
+            }
+        }
+
+        public async override Task OnAppearing()
+        {
+            await base.OnAppearing();
+            ValidationErrors.Clear();
         }
 
         public Command UpdateCommand { get; }
@@ -23,6 +40,15 @@ namespace BeeJee.Xamarin.App.ViewModels
 
         private void OnUpdate()
         {
+            if (_pagerPageSize <= 0)
+            {
+                ValidationErrors.Add(new ValidationError()
+                {
+                    PropertyName = nameof(PagerPageSize),
+                    ErrorMessage = "Неверное значение для размера страницы"
+                });
+                return;
+            }
             App.SettingsStore.PageSize.SetValue(_pagerPageSize);
         }
 
